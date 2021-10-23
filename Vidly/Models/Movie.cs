31ef803eec5a0b30e0ebb79404bfdfb4d543e686
F.e <effigub@gmail.com>
2021-10-23 13:29:39 +0300

    Added DataAnnotations To Movie

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+
         public Genre Genre { get; set; }
+
         public byte GenreId { get; set; }
+
+        [Display(Name = "Release Date")]
         public string ReleaseDate { get; set; }
+
         public string DateAdded { get; set; }
+
+        [Display(Name = "Number In Stock")]
+        [Range(1, 30)]
         public int NumberInStock { get; set; }
     }
 }
