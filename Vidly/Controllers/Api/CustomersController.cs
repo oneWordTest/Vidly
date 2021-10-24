5435630c3733def082903199d0a36453efb8d553
F.e <effigub@gmail.com>
2021-10-24 14:30:08 +0300

    Changed api action GetCustomer to IHttpActionResult

--- Vidly/Controllers/Api/CustomersController.cs
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -26,15 +26,14 @@ namespace Vidly.Controllers.Api
         }
 
         // GET /api/customer/1
-        public CustomerDto GetCustomer(int id)
+        public IHttpActionResult GetCustomer(int id)
         {
             var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
-
             if (customer == null)
-                throw new HttpResponseException(HttpStatusCode.NotFound);
+                return NotFound();
 
-            return Mapper.Map< Customer ,CustomerDto>(customer);
+            return Ok(Mapper.Map< Customer ,CustomerDto>(customer));
         }
 
         // POST /api/customers
