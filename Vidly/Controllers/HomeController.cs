aec036115841d7eff7cd869b59a5edb7ac03e9c8
F.e <effigub@gmail.com>
2021-10-30 13:16:51 +0300

    Disabled Session

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace Vidly.Controllers
     public class HomeController : Controller
     {
         //[OutputCache(Duration = 50, Location = System.Web.UI.OutputCacheLocation.Server, VaryByParam = "*")]
+        //[OutputCache(Duration = 0, VaryByParam = "*", NoStore = true)]
         public ActionResult Index()
         {
             return View();
